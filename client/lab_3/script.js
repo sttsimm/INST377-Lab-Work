25c16e40c7a2fb78c8640737130311286a4fd6ba
sttsimm <stsimmer@terpmail.umd.edu>
2023-02-22 09:38:59 -0500

    update

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -42,6 +42,13 @@ function moveToNextSlide() {
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
+  if(slidePosition === 0){
+    slidePosition = totalSlides - 1
+  }else{
+    slidePosition -= 1;
+  }
+
+
   // add your code in here for when you click the "prev" button
   /*
     add an if statement here that checks
