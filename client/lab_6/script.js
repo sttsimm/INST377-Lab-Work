e5edce6876d6ef8d81b3f10b4e61f094d8290db5
sttsimm <stsimmer@terpmail.umd.edu>
2023-03-19 13:17:41 -0400

    update

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -55,6 +55,7 @@ async function mainEvent() { // the async keyword means we can make API requests
 
     console.log(formProps); 
     const newList = filterList(currentList, formProps.resto); 
+    injectHTML(currentList);
     console.log(newList); 
 
   })
